6e3b9a88f3ec472e7456fdd7eb0858a5120bfe97
LinTui <1142396691@qq.com>
2020-03-18 18:05:57 +0800

    项目基础架构构建

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import Router from 'vue-router'
 Vue.config.productionTip = false
-
+Vue.use(Router)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
