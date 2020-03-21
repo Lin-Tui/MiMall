8662825118a3548eea8e68434c8bcdcffd7ca8d5
LinTui <1142396691@qq.com>
2020-03-21 19:20:25 +0800

    Session Storage封装

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,18 @@
 </template>
 
 <script>
+import storage from './storage' 
 export default {
   name: 'App',
   components: {
+  },
+  data() {
+    return {
+
+    }
+  },
+  mounted() {
+    
   }
 }
 </script>
---
+++ src/storage/index.js
@@ -0,0 +1,36 @@
+const STORAGE_KEY = 'mall';
+export default{
+    getStorge() {
+        return JSON.parse(window.sessionStorage.getItem(STORAGE_KEY) || '{}');
+    },
+    getItem(key, module_name) {
+        if (module_name) {
+            let val = this.getItem(module_name);
+            if (val) {
+                return val[key]
+            }
+        }
+        return this.getStorge()[key]
+    },
+    setItem(key, value, module_name) {
+        if (module_name) {
+            let val = this.getItem(module_name);
+            val[key] = value;
+            this.setItem(module_name,val)
+        } else {
+            let val = this.getStorge();
+            val[key] = value;
+            window.sessionStorage.setItem(STORAGE_KEY, JSON.stringify(val))
+        }
+
+    },
+    clear(key, module_name) {
+        let val = this.getStorge();
+        if (module_name) {
+            delete val[module_name][key]
+        } else {
+            delete val[key]
+        }
+        window.sessionStorage.setItem(STORAGE_KEY, JSON.stringify(val))
+    }
+}
