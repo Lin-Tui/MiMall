07d18e500041964a1a63efac2eb7eabb0c3e204a
LinTui <1142396691@qq.com>
2020-03-22 15:53:42 +0800

    接口环境设置

--- src/main.js
+++ src/main.js
@@ -1,7 +1,26 @@
 import Vue from 'vue'
-import App from './App.vue'
 import router from './router'
-Vue.config.productionTip = false
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import App from './App.vue'
+import env from './env'
+
+axios.defaults.baseURL = '/api';
+axios.defaults.timeout = 8000;
+axios.interceptors.response.use(function(response) {
+  let res = response.data;//这个response不是接口返回，而是axios封装给我们的。response.data才是接口返回值。
+  if (res.status == 0) {
+    return res.data;
+  } else if (res.status == 10) {
+    window.location.href = '/#/login';
+  } else {
+    alert(res.msg);
+  }
+});
+
+Vue.use(VueAxios, axios);
+Vue.config.productionTip = true;
+
 new Vue({
   router,
   render: h => h(App),
--- src/storage/index.js
+++ src/storage/index.js
@@ -27,6 +27,7 @@ export default{
     clear(key, module_name) {
         let val = this.getStorge();
         if (module_name) {
+            if (!val[module_name]) return;
             delete val[module_name][key]
         } else {
             delete val[key]
