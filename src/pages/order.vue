fe706fdfde675ccd3c26eb932a1bd9fe1a78d608
LinTui <1142396691@qq.com>
2020-04-07 14:02:30 +0800

    订单确认页面实现

--- src/pages/order.vue
+++ src/pages/order.vue
@@ -1,18 +1,44 @@
 <template>
     <div>
-        <order-header></order-header>
+        <order-header v-bind:title="title">
+            <template v-slot:tip>
+                <span>{{tip}}</span>
+            </template>
+        </order-header>
         <router-view></router-view>
+        <ServiceBar></ServiceBar>
         <nav-footer></nav-footer>
     </div>
 </template>
 <script>
 import OrderHeader from './../components/OrderHeader';
+import ServiceBar from './../components/ServiceBar';
 import NavFooter from './../components/NavFooter';
 export default {
     name: 'order',
     components: {
         OrderHeader,
-        NavFooter
+        NavFooter,
+        ServiceBar
+    },
+    data() {
+        return {
+            title: '',
+            tip: ''
+        }
+    },
+    mounted() {
+       let path = this.$route.path;
+       if (path == '/order/confirm') {
+           this.title = '订单确认';
+           this.tip = '请认真填写收货地址';
+       }  else if (path == '/order/list') {
+           this.title = '订单列表';
+           this.tip = '请谨防钓鱼链接或咋骗电话，了解更多';           
+       } else if (path == '/order/pay') {
+           this.title = '订单支付';
+           this.tip = '请谨防钓鱼链接或咋骗电话，了解更多';           
+       }
     }
 }
 </script>
