91aeae7fdc249c1c1f6436f6836f1f227e43c3e9
LinTui <1142396691@qq.com>
2020-04-07 19:36:18 +0800

    订单结算功能实现

--- src/main.js
+++ src/main.js
@@ -31,6 +31,10 @@ axios.interceptors.response.use(function(response) {
     Message.warning(res.msg);
     return Promise.reject(res);
   }
+}, (error) => {
+    let res = error.response;
+    Message.warning(res.data.message);
+    return Promise.reject(error)
 });
 
 Vue.use(VueAxios, axios);
